598df0769a8f6729fe3c949d53e4332aa5d79d4f
DOALA0155 <kyleshouki@gmail.com>
2020-04-12 12:13:48 +0900

    change detection.py

--- detection.py
+++ detection.py
@@ -2,16 +2,31 @@ import cv2
 import face_recognition
 
 video_capture = cv2.VideoCapture(0)
+eye_cascade = cv2.CascadeClassifier("../Cv2DetectionData/haarcascade_eye.xml")
 
 face_locations = []
 
 while True:
-    ret, frame = video_capture.read()
-    rgb_frame = frame[:, :, ::-1]
-    face_locations = face_recognition.face_locations(rgb_frame)
-    for top, right, bottom, left in face_locations:
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-    cv2.imshow("Video", frame)
+    ret, img = video_capture.read()
+    img = cv2.flip(img, 1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    rgb_img = img[:, :, ::-1]
+
+    face_locations = face_recognition.face_locations(rgb_img)
+
+    for top, left, bottom, right in face_locations:
+        cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        roi_color = img[top: bottom, right: left]
+
+        roi_gray = gray[top: bottom, right: left]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
+    cv2.imshow("Image", img)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
